4ec2b23eb891926b386a3877a921552029928d35
Maikel Wever <maikelwever@gmail.com>
2020-05-24 22:42:12 +0200

    extract manifest in lzma format

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@
 #[macro_use] extern crate error_chain;
 
 extern crate byteorder;
+extern crate lzma_rs;
+extern crate math;
 
 use byteorder::{ReadBytesExt, BigEndian};
+use lzma_rs::lzma_decompress;
+use math::round;
 
 use std::fs::File;
 use std::io;
-use std::io::{Seek, SeekFrom, Read, Write};
+use std::io::{Cursor, Seek, SeekFrom, Read, BufReader};
 
 
 error_chain!{
@@ -28,12 +32,14 @@ enum CompressionType {
 enum ArchiveFlags {
     RelativePaths,
     IgnoreCase,
-    AbsolutePaths
+    AbsolutePaths,
+    Unknown
 }
 
 
 #[derive(Debug)]
 struct FileEntry {
+    name: String,
     name_digest: [u8; 16],
     index_list_size: u32,
     length: u64,
@@ -81,7 +87,8 @@ struct PSArc {
 }
 
 impl PSArc {
-    fn open(file: &mut File) -> Result<Self> {
+    fn open(file: &mut BufReader<File>) -> Result<Self> {
+        let empty_string = "".to_string();
         let magic = file.read_u32::<BigEndian>()?;
         if magic != 0x50534152 {
             return Err(Error::from("Invalid magic"));
@@ -109,7 +116,7 @@ impl PSArc {
         let block_size = match file.read_u32::<BigEndian>()? {
             65536 => BlockSizeType::U16,
             16777216 => BlockSizeType::U24,
-            4294967296 => BlockSizeType::U32,
+            4294967295 => BlockSizeType::U32,
             i => {
                 return Err(Error::from(format!("Invalid block size type {}", i)))
             }
@@ -121,7 +128,8 @@ impl PSArc {
                     1 => ArchiveFlags::IgnoreCase,
                     2 => ArchiveFlags::AbsolutePaths,
                     _ => {
-                        return Err(Error::from(format!("Invalid archive flags {}", value)));
+                        println!("Invalid archive flags {}", value);
+                        ArchiveFlags::Unknown
                     }
                 }
             },
@@ -137,7 +145,7 @@ impl PSArc {
             let index_list_size = file.read_u32::<BigEndian>()?;
             let length = file.read_uint::<BigEndian>(5)?;
             let offset = file.read_uint::<BigEndian>(5)?;
-            entries.push(FileEntry { name_digest, index_list_size, length, offset });
+            entries.push(FileEntry { name: empty_string.clone(), name_digest, index_list_size, length, offset });
         }
 
         let current_pos = file.seek(SeekFrom::Current(0))?;
@@ -148,11 +156,28 @@ impl PSArc {
             block_sizes.push(file.read_uint::<BigEndian>(block_size.get_bytecount())?);
         }
 
-        Ok(Self {
+
+        let mut i = Self {
             version_minor, version_major, compression_type, 
             toc_length, toc_entry_size, toc_entry_count,
             block_size, archive_flags, entries, block_sizes
-        })
+        };
+        i.parse_manifest(file)?;
+        return Ok(i);
+    }
+
+    fn parse_manifest(&mut self, file: &mut BufReader<File>) -> Result<()> {
+        let mut c = Cursor::new(Vec::<u8>::new());
+        self.print_file(file, &mut c, 0)?;
+        //let reader = BufReader::new(c);
+        let mut d = String::new();
+        c.read_to_string(&mut d)?;
+        let mut lines: Vec<&str> = d.lines().collect();
+        lines.insert(0, "manifest.txt");
+        for (i, line) in lines.iter().enumerate() {
+            self.entries[i].name = line.to_string();
+        }
+        Ok(())
     }
 
     fn print_filelist(&self) {
@@ -172,30 +197,32 @@ impl PSArc {
         eprintln!("Amount of blocks registered:\t{}", self.block_sizes.len());
     }
 
-    fn print_file(&self, file: &mut File, index: usize) -> Result<()> {
+    fn print_file<W: io::Write>(&self, file: &mut BufReader<File>, out: &mut W, index: usize) -> Result<()> {
         let entry_details = &self.entries[index];
         eprintln!("{:?}", entry_details);
 
         let current_index: usize = entry_details.index_list_size as usize;
         let blockdetail = self.block_sizes[current_index];
-        eprintln!("Block detail stat: {}", blockdetail);
+        let total_bytes = blockdetail * self.block_size.get_bitcount() as u64;
+        let blocks = round::ceil(entry_details.length as f64 / self.block_size.get_bitcount() as f64, 0) as u64;
 
         file.seek(SeekFrom::Start(entry_details.offset))?;
-        let mut stdout = io::stdout();
 
-        if blockdetail == entry_details.length {
+        if total_bytes == entry_details.length {
             eprintln!("Compressed length is the same as original length.");
-            let mut datastream = file.take(entry_details.length);
-            io::copy(&mut datastream, &mut stdout)?;
+            let filesize = blocks * self.block_size.get_bitcount();
+            let mut datastream = file.take(filesize);
+            io::copy(&mut datastream, out)?;
         } else {
             let header = file.read_u16::<BigEndian>()?;
             eprintln!("Header value: {:X}", header);
             file.seek(SeekFrom::Start(entry_details.offset))?;
-            let mut datastream = file.take(blockdetail);
-            io::copy(&mut datastream, &mut stdout)?;
+            for _ in 0..blocks {
+                let mut datastream = file.take(self.block_size.get_bitcount());
+                lzma_decompress(&mut datastream, out).unwrap();
+            }
         }
 
-        stdout.flush()?;
         Ok(())
     }
 }
@@ -209,15 +236,15 @@ fn main() {
     ).get_matches();
 
     let filename = matches.value_of("file").unwrap();
-    let mut file_obj = match File::open(filename) {
+    let file_obj = match File::open(filename) {
         Ok(file) => file,
         Err(e) => panic!(e)
     };
-    let psarc = match PSArc::open(&mut file_obj) {
+    let mut reader = BufReader::new(file_obj);
+    let psarc = match PSArc::open(&mut reader) {
         Ok(psarc) => psarc,
         Err(e) => panic!("{:?}", e)
     };
     psarc.print_details();
-    //psarc.print_filelist();
-    psarc.print_file(&mut file_obj, 3).unwrap();
+    psarc.print_filelist();
 }
